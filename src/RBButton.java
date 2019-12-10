2752c54d7a982116e90c40cf2341c0b710b38bd1
林恩廷 <lin.enting@icloud.com>
2019-12-10 16:52:21 +0900

    add RBButton

---
+++ src/RBButton.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JLabel;
+
+public class RBButton extends MouseAdapter {
+		public void mouseEntered(MouseEvent e) {
+			JLabel l = (JLabel) e.getSource();
+			l.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+			l.setForeground(Color.red);
+		}
+		public void mouseExited(MouseEvent e) {
+			JLabel l = (JLabel) e.getSource();
+			l.setForeground(Color.black);
+		}
+}
