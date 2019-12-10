edab6475fade8051877fe9b0f01b276e32a4147b
林恩廷 <lin.enting@icloud.com>
2019-12-10 17:10:56 +0900

    add RBListener

--- src/RBListener.java
+++ src/RBListener.java
@@ -5,14 +5,16 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.JLabel;
 
-public class RBListener extends MouseAdapter {
-		public void mouseEntered(MouseEvent e) {
-			JLabel l = (JLabel) e.getSource();
-			l.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-			l.setForeground(Color.red);
-		}
-		public void mouseExited(MouseEvent e) {
-			JLabel l = (JLabel) e.getSource();
-			l.setForeground(Color.black);
-		}
+public abstract class RBListener extends MouseAdapter {
+	public void mouseEntered(MouseEvent e) {
+		JLabel l = (JLabel) e.getSource();
+		l.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		l.setForeground(Color.red);
+	}
+	public void mouseExited(MouseEvent e) {
+		JLabel l = (JLabel) e.getSource();
+		l.setForeground(Color.black);
+	}
+	
+	public abstract void mouseClicked(MouseEvent e);
 }
