4f32846687d6e72cb7fcaf053079bbeaa3df9788
林恩廷 <lin.enting@icloud.com>
2019-12-30 16:14:13 +0900

    tmp

--- src/HotelfunctionUI.java
+++ src/HotelfunctionUI.java
@@ -13,42 +13,42 @@ import javax.swing.SwingUtilities;
 import javax.swing.border.MatteBorder;
 
 public class HotelfunctionUI extends JPanel {
-	final private int hotelfunctionWidth = 500, hotelfunctionHeight = 200;
-	final private Dimension hotelfunctionCenter = new Dimension(Menu.frameWidth / 2, Menu.frameHeight / 2);
+	private static final long serialVersionUID = 1L;
+	
+	final private int hotelFunctionWidth = 500, hotelFunctionHeight = 200;
+	final private Dimension hotelFunctionCenter = new Dimension(HotelPreference.frameWidth / 2, HotelPreference.frameHeight / 2);
 	private JLayeredPane layeredPane = new JLayeredPane();
 	private JPanel hotelfunction = new JPanel();
 	
 	private void initPanel() {
 		setLayout(new GridLayout(1, 1));
 		setOpaque(false);
-		setPreferredSize(new Dimension(Menu.frameWidth, Menu.frameHeight));
+		setPreferredSize(new Dimension(HotelPreference.frameWidth, HotelPreference.frameHeight));
 	}
 	
 	private void initLayerPane() {
-		layeredPane.setPreferredSize(new Dimension(Menu.frameWidth, Menu.frameHeight));
-		Menu.background.setIcon(new ImageIcon("images/Menu/hotelbackground.jpg"));
-		Menu.background.setBounds(0, 0, Menu.frameWidth, Menu.frameHeight);
-		layeredPane.add(Menu.background, new Integer(0));
+		layeredPane.setPreferredSize(new Dimension(HotelPreference.frameWidth, HotelPreference.frameHeight));
+		HotelPreference.background.setIcon(new ImageIcon("images/Menu/hotelbackground.jpg"));
+		layeredPane.add(HotelPreference.background, new Integer(0));
 		layeredPane.add(hotelfunction, new Integer(1));
 		add(layeredPane);
 	}
 	
 	private void initHotelfunction() {
-		hotelfunction.setBounds(hotelfunctionCenter.width - (hotelfunctionWidth / 2),
-				hotelfunctionCenter.height - (hotelfunctionHeight / 2), hotelfunctionWidth, hotelfunctionHeight);
+		hotelfunction.setBounds(hotelFunctionCenter.width - (hotelFunctionWidth / 2),
+				hotelFunctionCenter.height - (hotelFunctionHeight / 2), hotelFunctionWidth, hotelFunctionHeight);
 		
 		// go to SearchUI
 		JLabel searchText = new JLabel("SEARCH", JLabel.CENTER);
 		searchText.setFont(new Font("Dialog", Font.BOLD, 36));
 		searchText.addMouseListener( new RBListener() {
-					public void mouseClicked(MouseEvent e) {
-						HotelfunctionUI.this.setVisible(false);
-						JFrame root = (JFrame) SwingUtilities.getRoot(HotelfunctionUI.this);
-						root.setContentPane(new SearchUI());
-						searchText.setForeground(Color.BLACK);
-					}
+				public void mouseClicked(MouseEvent e) {
+					HotelfunctionUI.this.setVisible(false);
+					JFrame root = (JFrame) SwingUtilities.getRoot(HotelfunctionUI.this);
+					root.setContentPane(new SearchUI());
+					searchText.setForeground(Color.BLACK);
 				}
-			);
+			});
 		
 		// go to ReserveUI
 		JLabel reserveText = new JLabel("RESERVE", JLabel.CENTER);
@@ -67,6 +67,18 @@ public class HotelfunctionUI extends JPanel {
 		// go to InquiryUI
 		JLabel inquiryText = new JLabel("INQUIRY", JLabel.CENTER);
 		inquiryText.setFont(new Font("Dialog", Font.BOLD, 36));
+		inquiryText.addMouseListener( new RBListener() {
+					public void mouseClicked(MouseEvent e) {
+						layeredPane.remove(hotelfunction);
+						HotelfunctionUI.this.setVisible(false);
+						JFrame root = (JFrame) SwingUtilities.getRoot(HotelfunctionUI.this);
+						root.setContentPane(new InquiryUI());
+						validate();
+						repaint();
+						inquiryText.setForeground(Color.BLACK);
+					}
+				}
+			);
 		
 		// go to RegistrationUI
 		JLabel logoutText = new JLabel("LOGOUT", JLabel.CENTER);
